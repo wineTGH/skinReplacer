c7f3148392bf8e8093512bac5d775edd4b8a13ce
wineTGH <66317493+wineTGH@users.noreply.github.com>
2020-06-13 16:41:31 +1000

    deleted debug info

--- skinReplace.bat
+++ skinReplace.bat
@@ -1,18 +1,16 @@
-rem Инициализация скрипта
 @echo off
 SetLocal EnableExtensions EnableDelayedExpansion
 title skinReplace v0.5
 color 1F
-rem программа 
+
+echo Made by wineT 
 md "%APPDATA%\.minecraft\resourcepacks\skinReplace %DATE%\assets\minecraft\textures\entity"
 copy /y resources\pack.png "%APPDATA%\.minecraft\resourcepacks\skinReplace %DATE%"
 move skin\steve.png "%APPDATA%\.minecraft\resourcepacks\skinReplace %DATE%\assets\minecraft\textures\entity\steve.png"
 choice /c 12345 /m "choose your minecraft version (1. <1.9, 2. 1.9, 3. 1.11 - 1.12, 4. 1.13 - 1.14, 5. 1.15)"
-echo mcmeta = mcmeta%ERRORLEVEL%
 GOTO mcmeta%ERRORLEVEL%
 
 :next
-echo mcmeta = %mcmeta%
 rem Код, для замены строки, был написан (или украден, я хз) Евгением с Хабр'а (@tw1ggyz). Огромное спасибо ему :)
 setlocal enabledelayedexpansion
 Set infile=pack.info
